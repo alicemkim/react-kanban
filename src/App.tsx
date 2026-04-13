9afdab8617e31ff147733387384bfcd0b60a2f2e
Alice Kim <alicekimtech@gmail.com>
2026-04-13 02:09:35 -0700

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import KanbanBoard from './components/KanbanBoard';
+
+function App() {
+  return (
+    <div className='mx-auto'>
+      <h3>Kanban Board</h3>
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,3 @@
+export default function KanbanBoard() {
+  return <div className='flex items-center justify-between'>hello world</div>;
+}
